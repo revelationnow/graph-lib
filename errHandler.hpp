8c83313772ffca131cda7964e6d0129e9468eeb1
revelationnow <anoop.ramakrishna@live.com>
2016-05-09 07:08:38 -0700

    Removed files to clean up directory structure

--- errHandler.hpp
+++
@@ -1,7 +0,0 @@
-/// \file errHandler.hpp
-#ifndef _ERRHANDLER_HPP_
-#define _ERRHANDLER_HPP_
-
-#define PRINT_TAG __FILE__<<"\t"<<__LINE__<<"\t"<<__DATE__<<"\t"<<__TIME__<<"\t"
-#define OUTPUT_MSG(TAG, MSG) cout<<PRINT_TAG<< #TAG" : " << MSG << endl;
-#endif
--- link.hpp
+++
@@ -1,8 +0,0 @@
-/// \file link.hpp
-#ifndef __LINK_HPP_
-#define __LINK_HPP_
-
-#include "errHandler.hpp"
-#include "common.hpp"
-using namespace std;
-#endif
